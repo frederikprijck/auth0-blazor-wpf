0880b8b2bc48fbd2881ff5ced43810dc1162ee12
Frederik <frederik.prijck@gmail.com>
2023-05-24 16:35:16 +0200

    Add default Blazor WPF application

---
+++ Auth0-Blazor-WPF/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Auth0_Blazor_WPF.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Auth0_Blazor_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddSingleton<WeatherForecastService>();
+
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+    }
+}
